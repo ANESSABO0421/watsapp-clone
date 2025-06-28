10d4bfa20adfe71bc173759aa0b9a40539068ad9
Anees <aneesaboo123@gmail.com>
2025-06-28 16:10:39 +0530

    color

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -4,7 +4,7 @@ const Footers = () => {
   return (
     <div>
         <section className='h-[400px] bg-[#111B21] rounded-tl-[50px] rounded-tr-[50px] flex gap-5 justify-center '>
-            <div className='logo flex flex-col bg-red-500'>
+            <div className='logo flex flex-col bg-blue-500'>
                 <img src="watlogo.svg" alt="" className='h-[30px]'/>
             </div>
         </section>
